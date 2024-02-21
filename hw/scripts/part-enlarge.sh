0cdee1f91a677a5db42664b564a289d99e84be11
Owen Osborn <owenosborn@gmail.com>
2024-02-20 22:41:12 -0500

    update disk name

--- hw/scripts/part-enlarge.sh
+++ hw/scripts/part-enlarge.sh
@@ -10,11 +10,12 @@ partprobe
 sync
 
 echo "-------------make new fs..."
-mkfs.vfat -F 32 -n 201PP /dev/mmcblk1p2
+mkfs.vfat -F 32 -n KALEIDOLOOP /dev/mmcblk1p2
 
 echo "-------------adding pd patch..."
 mount /dev/mmcblk1p2 /sdcard
 cp -r /home/root/app/pd /sdcard/
+mkdir -p /sdcard/recordings
 
 touch /etc/app-sdcard-format.lock
 sync
--- hw/scripts/part-shrink.sh
+++ hw/scripts/part-shrink.sh
@@ -1,10 +1,15 @@
+echo "------------preparing to re partition, shutting things down"
 killall pd
+killall app
 rmmod g_mass_storage
 umount /dev/mmcblk1p2
+echo "-----------re partition"
 sfdisk --no-reread /dev/mmcblk1 < part-small.dump 
 sync
+echo "-----------running partprobe"
 partprobe
 sync
+echo "-----------zero ing"
 dd if=/dev/zero of=/dev/mmcblk1p2 
 rm /etc/app-sdcard-format.lock
 sync
